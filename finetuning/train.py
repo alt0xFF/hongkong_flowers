db37b919a27670512ec225567cb24817d1fb51b3
Joseph Cheng <indiejoseph@gmail.com>
2017-10-02 14:47:05 +0800

    Update finetuning

--- finetuning/train.py
+++ finetuning/train.py
@@ -7,10 +7,11 @@ import math
 import glob
 import numpy as np
 import keras
-from keras.applications import ResNet50
+from keras.applications.resnet50 import ResNet50, preprocess_input
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
+
 from keras.models import Sequential, Model
 from keras.layers import Dropout, Flatten, Dense
 
@@ -44,27 +45,28 @@ def get_class_weight(d):
 def get_callbacks(weights_path, patience=30, monitor='val_loss'):
     early_stopping = EarlyStopping(verbose=1, patience=patience, monitor=monitor)
     model_checkpoint = ModelCheckpoint(weights_path, save_best_only=True, save_weights_only=True, monitor=monitor)
-    reduce_leanring_rate = ReduceLROnPlateau(monitor="val_acc", verbose=1)
+    reduce_leanring_rate = ReduceLROnPlateau(monitor="val_loss", factor=0.2, patience=5, min_lr=0.001)
     return [reduce_leanring_rate, early_stopping, model_checkpoint]
 
 # path to the model weights files.
-weights_path = 'trained/fine-tuned-resnet50-weights.h5'
+weights_path = 'pretrained_models/fine-tuned-resnet50-weights.h5'
 
 # dimensions of our images.
 img_width, img_height = 224, 224
 
-train_data_dir = 'data/oxford102/train'
-validation_data_dir = 'data/oxford102/valid'
-test_data_dir = 'data/oxford102/test'
+train_data_dir = 'dataset/oxford102/train'
+validation_data_dir = 'dataset/oxford102/valid'
+test_data_dir = 'dataset/oxford102/test'
 classes = sorted([o for o in os.listdir(train_data_dir) if os.path.isdir(os.path.join(train_data_dir, o))])
 epochs = 1000
-batch_size = 32
+batch_size = 50
 nb_classes = len(classes)
-patience = 20
+patience = 30
 class_weight = get_class_weight(train_data_dir)
 
-# build the VGG16 network
 model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+nfreezed_layers = 80
+
 print('Model loaded.')
 
 # set the first 80 layers (up to the last conv block)
@@ -72,7 +74,7 @@ print('Model loaded.')
 for layer in model.layers:
     layer.trainable = False
 
-for layer in model.layers[80:]:
+for layer in model.layers[nfreezed_layers:]:
     layer.trainable = True
 
 # build a classifier model to put on top of the convolutional model
@@ -80,12 +82,7 @@ y = model.output
 y = Flatten()(y)
 y = Dropout(0.5)(y)
 
-# now the shape = (batch_size, 2048)
-y = Dense(2048, activation='elu', name='fc1')(y)
-y = Dropout(0.5)(y)
-y = Dense(1024, activation='elu', name='fc2')(y)
-y = Dropout(0.5)(y)
-y = Dense(512, activation='elu', name='fc3')(y)
+y = Dense(1024, activation='elu', name='fc1')(y)
 y = Dropout(0.5)(y)
 
 predictions = Dense(nb_classes, activation='softmax', name='predictions2')(y)
@@ -94,20 +91,21 @@ model = Model(model.input, predictions, name='resnet50')
 
 # and a very slow learning rate.
 model.compile(loss='categorical_crossentropy',
-              optimizer=optimizers.Adam(lr=1e-4),
+              optimizer=optimizers.SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True),
               metrics=['accuracy'])
 
 # prepare data augmentation configuration
 train_datagen = ImageDataGenerator(
-    rotation_range=40,
+    preprocessing_function=preprocess_input,
+    rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
-    shear_range=0.2,
+    shear_range=0.1,
     zoom_range=0.2,
     horizontal_flip=True,
-    fill_mode='nearest')
+    vertical_flip=False)
 
-test_datagen = ImageDataGenerator()
+test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
